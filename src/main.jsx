28b6f4ca253e864660e68bad39c7c31d41cb9b4b
Goa-Matka <goamatka19@gmail.com>
2025-03-17 11:50:32 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -14,15 +14,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import "./index.css"; // ✅ Make sure to import Tailwind styles
+import './index.css'; // ✅ Make sure to import Tailwind styles
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-
-<App />
-
-
- 
-  </BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
