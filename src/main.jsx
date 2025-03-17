c983ae4cbec803c8d50c072c3d585fefe63abd1e
Goa-Matka <goamatka19@gmail.com>
2025-03-17 11:44:49 +0530

    main updated

--- src/main.jsx
+++ src/main.jsx
@@ -15,14 +15,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import "./index.css"; // ✅ Make sure to import Tailwind styles
-import {Provider} from 'react-redux'
-import store from '../../frontend/src/State/store';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-<Provider store={store}>
+
 <App />
-</Provider>
+
 
  
   </BrowserRouter>
